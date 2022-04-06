81a588fa9248a816033dbe6beb9157398c6823d8
bb2100 <albin.ekelunden@gmail.com>
2022-04-06 14:56:26 +0200

    Started with some funtions and two text files

---
+++ Passwordprogram.py
@@ -0,0 +1,21 @@
+
+class User:
+    def __init__(self, username:str,password:str) -> None:
+        self.username = username
+        self.password = password
+
+def login():
+    username = input("Skriv in ditt användarnamn:")
+
+
+    password = input("Skriv in ditt lösenord:")
+
+
+
+def register():
+    pass
+
+def main():
+    choice = input("För att logga in: skriv 1\nFör att registrera dig: skriv 2")
+    if (choice == "1")
+
