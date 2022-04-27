317599aad52da3cb4a38439ac0edb2bed01cd7b4
bb2100 <albin.ekelunden@gmail.com>
2022-04-27 15:04:52 +0200

    I have created the entire registe function

--- Passwordprogram.py
+++ Passwordprogram.py
@@ -19,7 +19,7 @@ def init_users():
     with open("users.txt", "r", encoding="utf-8") as file:
         for line in file.readlines():
             user = line.split("/")
-            passwords.append(User(user[0], user[1]))
+            passwords.append(User(user[0], user[1].rstrip("\n")))
     
     return passwords
 
@@ -35,6 +35,9 @@ def login(users):
     else:
         print("Username or password was incorrect.")
     
+def log():
+    pass
+
     # TODO:
     # - lägg till log för login
 
@@ -42,14 +45,29 @@ def login(users):
 
 def register():
     username = input("Enter your new username: ")
-
     password = input("Enter your new password: ")
+    with open("users.txt", "a", encoding="utf-8") as write:
+        write.write(f"{username}/{password}\n")
+    print ("Account successfully created!")
+    
 
 
 def main():
     users = init_users()
-    choice = input("För att logga in: skriv 1\nFör att registrera dig: skriv 2")
+    choice = input("För att logga in: skriv 1\nFör att registrera dig: skriv 2\nFör att avsluta: skriv något annat\nFör att kolla vilka som försökt logga in: skriv 3")
     if (choice == "1"):
+        login(users)
+        print ("Du är nu inloggad!")
+    
+    if (choice == "2"):
+        register()
+    
+    if (choice == "3"):
         login(users)
 
 
+    else:
+        raise SystemExit("Programmet Avslutades")
+
+if __name__ == "__main__":
+    main()
